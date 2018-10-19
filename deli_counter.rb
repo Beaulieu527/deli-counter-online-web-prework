f6a660eed849806987aeaaf381aedc5c02c049a7
beaulieudev <beaulieudev@gmail.com>
2018-10-19 22:20:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,6 @@ def take_a_number(katz_deli)
   #ticket = katz_deli.length + 1
   katz_deli << $ticket
   puts "Welcome, you are ticket number #{$ticket} . You are number #{katz_deli.length} in line."
-
   $ticket = katz_deli.length + 1
 end
 
