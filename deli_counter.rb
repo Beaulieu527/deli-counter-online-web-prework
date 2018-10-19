0d55bc9ce1c1009800670ccb9f26e507f318eb31
beaulieudev <beaulieudev@gmail.com>
2018-10-19 22:05:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    binding.pry
+    #binding.pry
     puts "Currently serving #{katz_deli.shift}."
   end
 end
