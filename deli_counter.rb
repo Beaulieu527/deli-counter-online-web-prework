536521a8e1c201657f85cceeca8d91b10f359a88
beaulieudev <beaulieudev@gmail.com>
2018-10-19 22:19:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,10 @@ $ticket = 1
 def take_a_number(katz_deli)
   #the katz deli array should be an array of ticket numbers, not names
   #ticket = katz_deli.length + 1
-  katz_deli << $ticket + 1
+  katz_deli << $ticket
   puts "Welcome, you are ticket number #{$ticket} . You are number #{katz_deli.length} in line."
+
+  $ticket = katz_deli.length + 1
 end
 
 def now_serving(katz_deli)
